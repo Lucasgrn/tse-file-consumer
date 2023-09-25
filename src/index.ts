74b919cfb3ceeeb7e26425c664022bd8d382a2c1
Lucasgrn <lucasgabriel.ogn@gmail.com>
2023-09-25 10:45:30 -0300

    feat: Cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { router } from './routes/route.js';
+import cors from 'cors'
 
 const app = express()
-
+app.use(cors())
 app.use(router)
 
 app.listen(3550, () => console.log('Running...'))
